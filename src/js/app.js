197c66daee821e370fbdfe208209ed3d3cc8b764
Laurascardozo <143517864+Laurascardozo@users.noreply.github.com>
2023-10-19 23:10:06 +0000

    Conditional profile modified

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,25 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
+          <h1>${variables.name ? variables.name : "name"} 
+          ${variables.lastName ? variables.lastName : "lastname"}</h1>
+          <h2>${variables.role ? variables.role : "role"}</h2>
+          <h3>${variables.city ? variables.city : "city"}, ${
+    variables.country ? variables.country : "country"
+  }</h3>
           <ul class="position-right">
-            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="ttps://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
