c53550c4797bbd3a01876aaeabb2cc9a75c3800a
Laurascardozo <143517864+Laurascardozo@users.noreply.github.com>
2023-10-25 04:34:44 +0000

    Social Media Position fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -39,7 +40,7 @@ function render(variables = {}) {
           <h3>${variables.city ? variables.city : "city"}, ${
     variables.country ? variables.country : "country"
   }</h3>
-          <ul class="position-right">
+          <ul class = ${variables.socialMediaPosition}> 
             <li><a href="https://twitter.com/${
               variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
